3099ff3dd179b948db622f516acd7e7f26b5d113
3nethz <ggthineth@gmail.com>
2024-08-15 01:06:16 +0530

    8/15

---
+++ src/components/DisplayHome.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./NavBar"
+
+const DisplayHome = () => {
+  return (
+    <>
+        <NavBar />
+        <div className="my-5 font-bold text-2xl">
+
+        </div>
+    </>
+  )
+}
+
+export default DisplayHome
