319de3167e791cfe3db7725b2e05f1ba5ea65bab
3nethz <ggthineth@gmail.com>
2024-08-16 09:12:54 +0530

    formatting sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,13 +2,15 @@ import { useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 
 const Sidebar = () => {
-
   const navigate = useNavigate();
 
   return (
     <div className="w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex">
       <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around">
-        <div onClick={() =>navigate('/')} className="flex items-center gap-3 pl-8 cursor-pointer">
+        <div
+          onClick={() => navigate("/")}
+          className="flex items-center gap-3 pl-8 cursor-pointer"
+        >
           <img className="w-6" src={assets.home_icon} alt="" />
           <p className="font-bold"> Home</p>
         </div>
@@ -31,12 +33,19 @@ const Sidebar = () => {
         <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
           <h1> Create Your First Playlist </h1>
           <p className="font-light"> It&apos;s Easy we will help you </p>
-          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">Create Playlist</button>
+          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">
+            Create Playlist
+          </button>
         </div>
         <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
           <h1> Let&apos;s Find Some Podcasts to Follow </h1>
-          <p className="font-light"> We&apos;ll keep you updated on new episodes </p>
-          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">Browse Podcasts</button>
+          <p className="font-light">
+            {" "}
+            We&apos;ll keep you updated on new episodes{" "}
+          </p>
+          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">
+            Browse Podcasts
+          </button>
         </div>
       </div>
     </div>
