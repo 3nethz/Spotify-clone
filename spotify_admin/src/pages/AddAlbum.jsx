bd6d82aa3476065e71e6c8453ca118c9d04e5d06
3nethz <ggthineth@gmail.com>
2024-08-17 16:28:32 +0530

    feat: Add album form with image upload and input fields

--- spotify_admin/src/pages/AddAlbum.jsx
+++ spotify_admin/src/pages/AddAlbum.jsx
@@ -1,10 +1,50 @@
+import { assets } from "../assets/assets";
 
 const AddAlbum = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <form className="flex flex-col items-start gap-8 text-gray-600">
+      <div className="flex flex-col gap-4">
+        <p>Upload Image</p>
+        <input type="file" id="image" accept="image/*" hidden />
+        <label htmlFor="image">
+          <img
+            src={assets.upload_area}
+            className="w-24 cursor-pointer"
+            alt=""
+          />
+        </label>
+      </div>
+      <div className="flex flex-col gap-2.5">
+        <p>Album Name</p>
+        <input
+          type="text"
+          className="bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,250px)]"
+          placeholder="Type Here"
+          required
+        />
+      </div>
+      <div className="flex flex-col gap-2.5">
+        <p>Album description</p>
+        <input
+          type="text"
+          className="bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw,250px)]"
+          placeholder="Type Here"
+          required
+        />
+      </div>
+      <div className="flex flex-col gap-3">
+        <p>Background Colour </p>
+        <input type="color" />
+      </div>
 
-export default AddAlbum
+      <button
+        className="text-base text-white bg-black py-2.5 px-14 cursor-pointer"
+        type="submit"
+      >
+        ADD
+      </button>
+    </form>
+  );
+};
+
+export default AddAlbum;
